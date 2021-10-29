4731f3d53c763023e6ded4f824da6c5653acfa68
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-10-29 09:57:02 -0300

    Extends/super/get&set

--- src/class/metodoPrivado.js
+++ src/class/metodoPrivado.js
@@ -8,5 +8,28 @@ class Person{
     speak(){
         return console.log(`Your name is ${this.#name} and your age is ${this.#age}`);
     }
+    set name(string){
+        this.#age = string;
+    }
+    get name(){
+        return this.#name;
+    }
+    set age (number){
+        this.#age = number;
+    }
+    get age(){
+        return this.#age;
+    }
+
 }
-module.exports = Person;
+class Alien extends Person {
+    #codelanguage
+    constructor({name, age, codelanguage}){
+        super({name, age});
+        this.#codelanguage = codelanguage;
+    }
+    stack(){
+        return console.log(`Hi ${this.name}, I see here that you're ${this.age} years old. Are you realy trying to be a ${this.#codelanguage} developer at this age??`);
+    }
+}
+module.exports = Alien;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 // let marco = new Person({name:'Marco',age : 21})._name;
 // console.log(marco);
 
-const Person = require('./class/metodoPrivado');
-let gui = new Person({name:'Guilherme', age: 21}).speak();
-console.log(gui);
+const Alien = require('./class/metodoPrivado');
+let alien1 = new Alien({name:'Guilherme', age: 21, codelanguage: 'Javascript'}).stack();
+alien1;
